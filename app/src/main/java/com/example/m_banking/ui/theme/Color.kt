c6f554d23049eb51657e5293dca340988fc9ead1
stanislav.tushinsky <stasbjo1212@gmail.com>
2024-06-25 09:39:13 +0300

    Initial commit

---
+++ app/src/main/java/com/example/m_banking/ui/theme/Color.kt
@@ -0,0 +1,19 @@
+package com.example.m_banking.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple80 = Color(0xFFD0BCFF)
+val PurpleGrey80 = Color(0xFFCCC2DC)
+val Pink80 = Color(0xFFEFB8C8)
+
+val Purple40 = Color(0xFF6650a4)
+val PurpleGrey40 = Color(0xFF625b71)
+val Pink40 = Color(0xFF7D5260)
+
+val ButtonBackground = Color(0xFF409CFF)
+val CardBackground = Color(0xFF1C1C1E)
+val CardSelectedBackground = Color(0xFF193053)
+val ExecutedTextColor = Color(0xFF52AB5B)
+val DeclinedTextColor = Color(0xFFF5675E)
+val InProgressTextColor = Color(0xFFF1BF37)
+val DetailsTextColor = Color(0xFFEBEBF5)
---
+++ app/src/main/java/com/example/m_banking/ui/theme/Type.kt
@@ -0,0 +1,47 @@
+package com.example.m_banking.ui.theme
+
+import androidx.compose.material3.Typography
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+import com.example.m_banking.R
+
+val Typography = Typography(
+    bodyLarge = TextStyle(
+        fontFamily = FontFamily(Font(R.font.lato_regular)),
+        fontWeight = FontWeight.Normal,
+        fontSize = 16.sp,
+        lineHeight = 24.sp,
+        letterSpacing = 0.5.sp
+    ),
+    titleMedium = TextStyle(
+        fontFamily = FontFamily(Font(R.font.lato_bold)),
+        fontWeight = FontWeight.Bold,
+        fontSize = 37.sp,
+        lineHeight = 45.sp,
+        letterSpacing = 0.sp
+    ),
+    titleLarge = TextStyle(
+        fontFamily = FontFamily(Font(R.font.lato_bold)),
+        fontWeight = FontWeight.Bold,
+        fontSize = 45.sp,
+        lineHeight = 55.sp,
+        letterSpacing = 0.sp
+    ),
+    titleSmall = TextStyle(
+        fontFamily = FontFamily(Font(R.font.lato_regular)),
+        fontWeight = FontWeight.Medium,
+        fontSize = 23.sp,
+        lineHeight = 29.sp,
+        letterSpacing = 0.sp
+    ),
+    labelSmall = TextStyle(
+        fontFamily = FontFamily(Font(R.font.lato_regular)),
+        fontWeight = FontWeight.Normal,
+        fontSize = 17.sp,
+        lineHeight = 24.sp,
+        letterSpacing = 0.sp
+    )
+)
