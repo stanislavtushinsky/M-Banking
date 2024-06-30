193d0288ab9cf00277f274fae56db2337c405088
stanislav.tushinsky <stasbjo1212@gmail.com>
2024-06-30 13:03:45 +0300

    Refactor code style

--- app/src/main/java/com/example/m_banking/presentation/navigation/AppNavigation.kt
+++ app/src/main/java/com/example/m_banking/presentation/navigation/AppNavigation.kt
@@ -2,7 +2,10 @@ package com.example.m_banking.presentation.navigation
 
 sealed class NavigationItem(val route: String) {
     data object Home : NavigationItem(route = "home_screen")
+
     data object AllTransactions : NavigationItem(route = "all_transactions")
+
     data object AddTransaction : NavigationItem(route = "add_transactions")
+
     data object DetailsTransaction : NavigationItem(route = "details_transaction")
 }
